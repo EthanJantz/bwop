6ef95080ad5365f97bd59fc19db91ab8c5e8fc9b
S M Mukarram Nainar <theone@sm2n.ca>
2025-08-24 15:34:46 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }: 
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nixd
+            pkgs.nixfmt-rfc-style
+
+            pkgs.bun
+          ];
+        };
+      }
+    );
+}
